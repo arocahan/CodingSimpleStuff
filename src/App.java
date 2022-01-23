0c59e8d5979c8af95fe84b2697483f5b8368af04
Arael <arael.roca@gmail.com>
2022-01-23 14:29:11 +0100

    Just a simple test

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[]args){
 
-        int [] TestArray1 = {4, 5, 6, 3, 7, 2};
+        int [] TestArray1 = {4, 5, 6, 3, 7, 2, 9};
 
         SimpleStuffs testObject = new SimpleStuffs();
         System.out.println(testObject.Addition(TestArray1));
